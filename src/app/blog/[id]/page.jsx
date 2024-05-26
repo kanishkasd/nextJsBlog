1a8400c92d0c96e361ee23d4f2b4c8310be5b35a
kanishkasd <kanishkasandaru2048@gmail.com>
2024-05-26 20:13:12 +0530

    add assets and lot more

---
+++ src/app/blog/[id]/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Id = ({ param }) => {
+  return <div>Blog Id: {param}</div>;
+};
+
+export default Id;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./footer.module.css";
+
+const Footer = () => {
+  return <div>©2023 Pilma. All rights reversed. </div>;
+};
+
+export default Footer;
