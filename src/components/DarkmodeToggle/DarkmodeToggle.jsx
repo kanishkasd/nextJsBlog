635224819f40b1a5b13deebe66897f4d1bd4f0ab
kanishkasd <kanishkasandaru2048@gmail.com>
2024-05-31 20:25:18 +0530

    styles toggle light button

--- src/components/DarkmodeToggle/DarkmodeToggle.jsx
+++ src/components/DarkmodeToggle/DarkmodeToggle.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import styles from "./DarkmodeToggle.module.css";
 
 const DarkmodeToggle = () => {
+  const mode = "dark";
   return (
     <div className={styles.container}>
       <div className={styles.icon}>🌙</div>
       <div className={styles.icon}>🌞</div>
-      <div className={styles.ball} />
+      <div
+        className={styles.ball}
+        style={mode === "light" ? { left: "2px" } : { right: "2px" }}
+      />
     </div>
   );
 };
