23c3a95b9bee57ed96e78e716445b0e4ced09a21
kanishkasd <kanishkasandaru2048@gmail.com>
2024-06-01 21:37:37 +0530

    implement promises sort of like practice one

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,51 +3,39 @@ import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
-const Blog = () => {
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("failed to fetch data");
+  }
+  return res.json();
+}
+
+const Blog = async () => {
+  const data = await getData();
   return (
     <div className={styles.mainContainer}>
-      <Link href="/blog/id" className={styles.container}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/12295008/pexels-photo-12295008.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            width={400}
-            height={400}
-            alt=""
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Title</h1>
-          <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
-      <Link href="/blog/id" className={styles.container}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/20508967/pexels-photo-20508967/free-photo-of-man-in-jacket-and-t-shirt.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            width={400}
-            height={400}
-            alt=""
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Title</h1>
-          <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
-      <Link href="/blog/id" className={styles.container}>
-        <div className={styles.imgContainer}>
-          <Image
-            src="https://images.pexels.com/photos/25039343/pexels-photo-25039343/free-photo-of-a-painting-of-a-blue-sky-with-wHite-clouds.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            width={400}
-            height={400}
-            alt=""
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Title</h1>
-          <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
+      {data.map((item) => {
+        return (
+          <Link href="/blog/id" className={styles.container} key={item.id}>
+            <div className={styles.imgContainer}>
+              <Image
+                src="https://images.pexels.com/photos/12295008/pexels-photo-12295008.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+                width={400}
+                height={400}
+                alt=""
+              />
+            </div>
+            <div className={styles.content}>
+              <h1 className={styles.title}>{item.title}</h1>
+              <p className={styles.desc}>{item.body}</p>
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 };
